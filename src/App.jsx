78b400ee9da31c74ba705998a53822b3065062d1
Gbrlbrgm <gabriel_bergamo@outlook.com>
2025-01-05 20:47:03 -0300

    Header estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Footer from './Components/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import Header from './Components/Header';
+import Login from './Components/Login/Login';
+import './App.css';
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
