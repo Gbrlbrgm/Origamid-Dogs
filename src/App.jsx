541d5a9a4e0c06aa3e4c1fedd3c916659f6d1ac5
Gbrlbrgm <gabriel_bergamo@outlook.com>
2025-01-06 19:28:56 -0300

    finalizadas as telas de login e contexto

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import Header from './Components/Header';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 import './App.css';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
